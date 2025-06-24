c121cba2664d78536a9d87791a87f09e7fb3d199
GarvTheCoder <garvsharma7895@gmail.com>
2025-06-24 11:50:44 +0530

    Add files via upload

---
+++ first_1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello\n";
+    int a=20;
+    int b=10;
+    cout<<a*b<<"\n";
+    cout<<a%b<<"\n";
+    cout<<a-b<<"\n";
+    cout<<a+b<<"\n";
+}
